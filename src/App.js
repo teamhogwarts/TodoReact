89f7f560edf85e563164ee18b994ab63c17a8ef4
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-06-26 13:18:59 +0200

    createToDo implemented

--- src/App.js
+++ src/App.js
@@ -2,104 +2,116 @@ import React, {Component} from 'react';
 
 import {ToDoItem} from "./components/ToDoItem";
 import {CreateToDoItem} from "./components/CreateToDoItem";
+import {Col, Input, Row} from "reactstrap";
+import Card from "reactstrap/es/Card";
+import CardBody from "reactstrap/es/CardBody";
+import CardTitle from "reactstrap/es/CardTitle";
+import Button from "reactstrap/es/Button";
 
 
 class App extends Component {
 
     state = {
-        todos : [],
+        todos: [],
         creator: '',
         text: ''
     }
 
-    creatorHandler(creatorData){
+    creatorHandler(creatorData) {
         this.setState({creator: creatorData})
-    }
+    };
 
-    textHandler(textData){
+    textHandler(textData) {
         this.setState({text: textData})
-    }
+    };
 
     componentDidMount() {
         this.getAllTodos()
     }
 
     render() {
-        return(
+        return (
             <div>
-                <CreateToDoItem/>
-            <h1>ToDo Liste</h1>
-                {this.state.todos.map((todo, i) => <ToDoItem key={i}
-                                                             creatorHandler={() => this.creatorHandler()}
-                                                             textHandler={() => this.textHandler()}
-                                                             createHandler={() => this.createToDoDTO()} handler={id => this.deleteToDoItem(id)} todo={todo}/>)}
+                <h1>Eingabe</h1>
+                <Row>
+                    <Col md="6">
+                        <Card body inverse color="dark">
+                            <CardBody>
+                                <CardTitle><strong>ToDo</strong></CardTitle>
+                                <Input type="text"
+                                       placeholder={'Enter creator...'}
+                                       onChange={e => this.creatorHandler(e.target.value)}/>
+                                <Input type="text"
+                                       placeholder={'Enter text...'}
+                                       onChange={e => this.textHandler(e.target.value)}/>
+                                <Button color="dark" onClick={() => this.createToDo()}>Create</Button>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+                <h1>ToDo Liste</h1>
+                {this.state.todos.map((todo, i) => <ToDoItem key={i} handler={id => this.deleteToDoItem(id)}
+                                                             todo={todo}/>)}
             </div>
         )
     }
 
-    async getAllTodos(){
-        try{
+    async getAllTodos() {
+        try {
             const response = await fetch("http://localhost:8080/toDo/toDos");
-            if(response.status === 200){
+            if (response.status === 200) {
                 const todoList = await response.json();
                 this.setState({todos: todoList});
-            }
-            else{
+            } else {
                 this.setState({todos: []});
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.error(e);
         }
 
     }
 
-    async deleteToDoItem(id){
+    async deleteToDoItem(id) {
         try {
             const request = new Request('http://localhost:8080/toDo/delete/' + id,
                 {method: 'DELETE'});
             const response = await fetch(request);
-            if(response.status !== 200){
+            if (response.status !== 200) {
                 throw new Error('Something went wrong on api server!');
-            }
-            else{
+            } else {
                 this.getAllTodos();
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e);
         }
     }
 
-    async createToDo(toDo){
+    async createToDo() {
         try {
+            const todo = {
+                creator: this.state.creator,
+                text: this.state.text
+            };
+
             const request = new Request('http://localhost:8080/toDo/create',
                 {
                     method: 'POST',
                     headers: new Headers({
                         'Content-Type': 'application/json'
                     }),
-                    body: JSON.stringify(toDo)
+                    body: JSON.stringify(todo)
                 });
 
             const response = await fetch(request);
-            if(response.status !== 200){
+
+            if (response.status !== 201) {
                 throw new Error('Something went wrong on api server!');
-            }
-            else{
+            } else {
                 this.getAllTodos();
             }
-        }
-        catch (e) {
+        } catch (e) {
             console.log(e);
         }
-    }
-
-    createToDoDTO(){
-        this.createToDo({
-            creator: this.state.creator,
-            text: this.state.text
-        }) ;
     }
 }
 
--- src/components/CreateToDoItem.js
+++ src/components/CreateToDoItem.js
@@ -5,20 +5,20 @@ import CardBody from "reactstrap/es/CardBody";
 import CardTitle from "reactstrap/es/CardTitle";
 import Button from "reactstrap/es/Button";
 
-export const CreateToDoItem = (createHandler, creatorHandler, textHandler) =>
+export const CreateToDoItem = (creatorHandler, textHandler, createHandler) =>
     <div>
         <h1>Eingabe</h1>
         <Row>
             <Col md="6">
                 <Card body inverse color="dark">
                     <CardBody>
-                        <CardTitle><strong>Conversation</strong></CardTitle>
+                        <CardTitle><strong>ToDo</strong></CardTitle>
                         <Input type="text"
                                placeholder={'Enter creator...'}
-                               onChange={e => creatorHandler(e.target.value)}/>
+                               onChange={e => () => creatorHandler(e.target.value)}/>
                         <Input type="text"
                                placeholder={'Enter text...'}
-                               onChange={e => textHandler(e.target.value)}/>
+                               onChange={e => () =>textHandler(e.target.value)}/>
                         <Button color="dark" onClick={() => createHandler()}>Create</Button>
                     </CardBody>
                 </Card>
