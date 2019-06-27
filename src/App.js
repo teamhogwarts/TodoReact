1297771fb40e4085e49befa45e6c0d9c7f7bd97e
Pascal Andermatt <pascal.andermatt@students.fhnw.ch>
2019-06-27 13:12:13 +0200

    final commit :D

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ export default class App extends Component {
         todos: [],
         creator: '',
         text: ''
-    }
+    };
 
-    textHandler = (inputVale) => {
-        this.setState({text: inputVale})
-    }
-    creatorHandler = (inputVale) => {
-        this.setState({creator: inputVale})
-    }
+    textHandler = (text) => {
+        this.setState({text: text})
+    };
+    creatorHandler = (creator) => {
+        this.setState({creator: creator})
+    };
 
     componentDidMount() {
         this.getAllTodos()
@@ -27,9 +27,9 @@ export default class App extends Component {
         return (
             <div>
                 <h1>Eingabe</h1>
-                <ToDoForm createButton={this.createToDo}
-                          textInput={e => this.textHandler(e)}
-                          creatorInput={e => this.creatorHandler(e)}
+                <ToDoForm createHandler={this.createToDo}
+                          textHandler={text => this.textHandler(text)}
+                          creatorHandler={creator => this.creatorHandler(creator)}
                 />
                 <h1>ToDo Liste</h1>
                 {this.state.todos.map((todo, i) => <ToDoItem key={i}
@@ -43,8 +43,9 @@ export default class App extends Component {
 
     async getAllTodos() {
         try {
-            const response = await fetch("http://localhost:8080/toDo/toDos");
-            if (response.status === 200) {
+            const response = await fetch('http://localhost:8080/toDo/toDos');
+
+            if (response.ok) {
                 const todoList = await response.json();
                 this.setState({todos: todoList});
             } else {
@@ -53,14 +54,15 @@ export default class App extends Component {
         } catch (e) {
             console.error(e);
         }
-
     }
 
     async deleteToDoItem(id) {
         try {
             const request = new Request('http://localhost:8080/toDo/delete/' + id,
                 {method: 'DELETE'});
+
             const response = await fetch(request);
+
             if (response.status !== 200) {
                 throw new Error('Something went wrong on api server!');
             } else {
@@ -72,31 +74,30 @@ export default class App extends Component {
     }
 
     createToDo = async () => {
-         try {
-             const todo = {
-                 creator: this.state.creator,
-                 text: this.state.text
-             };
+        try {
+            const todo = {
+                creator: this.state.creator,
+                text: this.state.text
+            };
 
-             const request = new Request('http://localhost:8080/toDo/create',
-                 {
-                     method: 'POST',
-                     headers: new Headers({
-                         'Content-Type': 'application/json'
-                     }),
-                     body: JSON.stringify(todo)
-                 });
+            const request = new Request('http://localhost:8080/toDo/create',
+                {
+                    method: 'POST',
+                    headers: new Headers({
+                        'Content-Type': 'application/json'
+                    }),
+                    body: JSON.stringify(todo)
+                });
 
-             const response = await fetch(request);
+            const response = await fetch(request);
 
-             if (response.status !== 201) {
-                 throw new Error('Something went wrong on api server!');
-             } else {
-                 this.getAllTodos();
-             }
-         } catch (e) {
-             console.log(e);
-         }
+            if (response.status !== 201) {
+                throw new Error('Something went wrong on api server!');
+            } else {
+                this.getAllTodos();
+            }
+        } catch (e) {
+            console.log(e);
+        }
     }
-
 }
